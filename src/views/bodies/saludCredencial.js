992f45625281a5d953034316948c61e93d68f7ba
BrunoMon <brunomon@hotmail.com>
2021-06-17 23:42:40 -0300

    arreglar mes en credencial

--- src/views/bodies/saludCredencial.js
+++ src/views/bodies/saludCredencial.js
@@ -18,205 +18,205 @@ const DATOS_COBERTURA = "coberturas.timeStamp";
 const ERROR_COBERTURA = "coberturas.errorTimeStamp";
 
 export class saludCredencialScreen extends connect(store, DATOS_COBERTURA, ERROR_COBERTURA, MEDIA_CHANGE, SCREEN)(LitElement) {
-	constructor() {
-		super();
-		this.hidden = true;
-		this.area = "body";
-		this.current = "";
-		this.idioma = store.getState().ui.idioma;
-		this.cobertura = null;
-	}
+    constructor() {
+        super();
+        this.hidden = true;
+        this.area = "body";
+        this.current = "";
+        this.idioma = store.getState().ui.idioma;
+        this.cobertura = null;
+    }
 
-	static get styles() {
-		return css`
-			${gridLayout}
-			${button}
+    static get styles() {
+        return css`
+            ${gridLayout}
+            ${button}
 			${select}
 
 			:host {
-				display: grid;
-				position: relative;
-				overflow-x: hidden;
-				overflow-y: auto;
-				padding: 0 !important;
-				background-image: url("https://app.uocra.org/images/credencialFondo.gif");
-				background-repeat: no-repeat;
-				background-position: center center;
-				background-size: cover;
-			}
-			:host([hidden]) {
-				display: none;
-			}
-			#cuerpo {
-				width: 100vw;
-				grid-gap: 0.5rem;
-				background-color: rgba(255 255 255 255) !important;
-				padding: 0 !important;
-				place-self: flex-start;
-				overflow-x: hidden;
-				overflow-y: auto;
+                display: grid;
+                position: relative;
+                overflow-x: hidden;
+                overflow-y: auto;
+                padding: 0 !important;
+                background-image: url("https://app.uocra.org/images/credencialFondo.gif");
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-size: cover;
+            }
+            :host([hidden]) {
+                display: none;
+            }
+            #cuerpo {
+                width: 100vw;
+                grid-gap: 0.5rem;
+                background-color: rgba(255 255 255 255) !important;
+                padding: 0 !important;
+                place-self: flex-start;
+                overflow-x: hidden;
+                overflow-y: auto;
 
-				justify-self: center;
-			}
-			#tituloTexto {
-				background-color: rgba(0, 0, 0, 0.4) !important;
-				align-self: self-start;
-				font-size: var(--font-header-h1-size);
-				font-weight: 900;
-				grid-template-columns: auto 1fr;
-				padding-bottom: 1rem;
-				color: var(--color-blanco);
-			}
-			#subTituloTexto {
-				width: 80%;
-				align-self: self-start;
-				font-size: var(--font-header-h1-size);
-				justify-self: center;
-				grid-template-columns: auto 1fr;
-				padding-bottom: 1rem;
-				color: var(--color-blanco);
-			}
-			#bullet {
-				fill: var(--color-blanco);
-				stroke: var(--color-verde-claro);
-			}
-			#bullet1 svg {
-				fill: var(--color-blanco);
-				stroke: var(--color-verde-claro);
-				height: 1.2rem;
-				width: 1.2em;
-			}
-			.detalle {
-				background-color: var(--color-rojo);
-				width: 10rem;
-				height: 10rem;
-				border-radius: 1rem;
-				justify-items: center;
-				align-content: center;
-				font-size: var(--font-header-h1-menos-size);
-				text-align: center;
-			}
-			.imagen {
-				cursor: pointer;
-			}
-			.imagen svg {
-				width: 3rem;
-				height: 3rem;
-			}
-			#credencial {
-				width: 85vw;
-				height: 65vw;
-				background-image: url("https://app.uocra.org/credencial/fondoCredencialDigital.svg");
-				background-repeat: no-repeat;
-				background-position: center center;
-				background-size: contain;
-				justify-self: center;
-				grid-gap: 0 !important;
-			}
-			#logo {
-				width: 40vw;
-				height: 18vw;
-				background-image: url("https://app.uocra.org/credencial/logo.png");
-				background-repeat: no-repeat;
-				background-position: bottom center;
-				background-size: contain;
-				justify-self: center;
-			}
-			.txtTarjeta {
-				color: white;
-				font-size: 5vw;
-			}
-		`;
-	}
-	render() {
-		if (store.getState().coberturas.tieneCobertura == "S") {
-			return html`
-				<div id="cuerpo" class="grid row">
-					<div id="tituloTexto" class="grid">
-						<div id="bullet">${SVGS["BULLET"]}</div>
-						<div id="solicitud">Credencial Digital</div>
-					</div>
-					<div style="padding:2rem"></div>
-					<div id="credencial" class="grid row">
-						<div id="logo"></div>
-						<div><hr /></div>
-						<div class="txtTarjeta" style="padding:0 0 0 4vw;">${store.getState().autorizacion.usuario ? store.getState().autorizacion.usuario.apellido + " " + store.getState().autorizacion.usuario.nombre : ""}</div>
-						<div class="txtTarjeta" style="padding:0 0 0 4vw;">${store.getState().autorizacion.usuario ? store.getState().autorizacion.usuario.documento : ""}</div>
-						<div><hr /></div>
-						<div class="txtTarjeta" style="padding:2vw 0 4vw 4vw;">Generada: ${new Date().getDate() + "-" + new Date().getMonth() + "-" + new Date().getFullYear()}</div>
-					</div>
-					<div id="subTituloTexto" class="grid">
-						<div id="bullet1">${SVGS["BULLET"]}</div>
-						<div id="solicitud">Se debe presentar DNI.</div>
-					</div>
-					<div id="subTituloTexto" class="grid">
-						<div id="bullet1">${SVGS["BULLET"]}</div>
-						<div id="solicitud">Presentar último recibo de sueldo.</div>
-					</div>
-				</div>
-			`;
-		} else {
-			if (store.getState().coberturas.tieneCobertura == "N") {
-				return html`<msgsincobertura-component texto="Volver"></msgsincobertura-component>; `;
-			}
-			if (store.getState().coberturas.tieneCobertura == "E") {
-				return html`<msgnoconeccion-component @click="${this.atras}" texto="Haga click volver" style="cursor:pointer"></msgnoconeccion-component>; `;
-			}
-		}
-	}
-	pdf(e) {
-		let archivo = e.currentTarget.item;
-		window.open(archivo, "_blank");
-		//location.href = archivo
-	}
-	atras() {
-		store.dispatch(goHistoryPrev());
-	}
-	stateChanged(state, name) {
-		if (name == SCREEN || name == MEDIA_CHANGE) {
-			this.mediaSize = state.ui.media.size;
-			this.hidden = true;
-			this.current = state.screen.name;
-			const haveBodyArea = isInLayout(state, this.area);
-			const SeMuestraEnUnasDeEstasPantallas = "-saludCredencial-".indexOf("-" + state.screen.name + "-") != -1;
-			if (haveBodyArea && SeMuestraEnUnasDeEstasPantallas) {
-				this.hidden = false;
-				store.dispatch(getCobertura({ tipo: state.autorizacion.usuario.tipoDocumento, documento: state.autorizacion.usuario.documento }));
-			}
-		}
-		if (name == DATOS_COBERTURA) {
-			this.cobertura = state.coberturas.entities;
-			this.update();
-		}
-		if (name == ERROR_COBERTURA) {
-			this.cobertura = 0;
-			this.update();
-		}
-	}
+                justify-self: center;
+            }
+            #tituloTexto {
+                background-color: rgba(0, 0, 0, 0.4) !important;
+                align-self: self-start;
+                font-size: var(--font-header-h1-size);
+                font-weight: 900;
+                grid-template-columns: auto 1fr;
+                padding-bottom: 1rem;
+                color: var(--color-blanco);
+            }
+            #subTituloTexto {
+                width: 80%;
+                align-self: self-start;
+                font-size: var(--font-header-h1-size);
+                justify-self: center;
+                grid-template-columns: auto 1fr;
+                padding-bottom: 1rem;
+                color: var(--color-blanco);
+            }
+            #bullet {
+                fill: var(--color-blanco);
+                stroke: var(--color-verde-claro);
+            }
+            #bullet1 svg {
+                fill: var(--color-blanco);
+                stroke: var(--color-verde-claro);
+                height: 1.2rem;
+                width: 1.2em;
+            }
+            .detalle {
+                background-color: var(--color-rojo);
+                width: 10rem;
+                height: 10rem;
+                border-radius: 1rem;
+                justify-items: center;
+                align-content: center;
+                font-size: var(--font-header-h1-menos-size);
+                text-align: center;
+            }
+            .imagen {
+                cursor: pointer;
+            }
+            .imagen svg {
+                width: 3rem;
+                height: 3rem;
+            }
+            #credencial {
+                width: 85vw;
+                height: 65vw;
+                background-image: url("https://app.uocra.org/credencial/fondoCredencialDigital.svg");
+                background-repeat: no-repeat;
+                background-position: center center;
+                background-size: contain;
+                justify-self: center;
+                grid-gap: 0 !important;
+            }
+            #logo {
+                width: 40vw;
+                height: 18vw;
+                background-image: url("https://app.uocra.org/credencial/logo.png");
+                background-repeat: no-repeat;
+                background-position: bottom center;
+                background-size: contain;
+                justify-self: center;
+            }
+            .txtTarjeta {
+                color: white;
+                font-size: 5vw;
+            }
+        `;
+    }
+    render() {
+        if (store.getState().coberturas.tieneCobertura == "S") {
+            return html`
+                <div id="cuerpo" class="grid row">
+                    <div id="tituloTexto" class="grid">
+                        <div id="bullet">${SVGS["BULLET"]}</div>
+                        <div id="solicitud">Credencial Digital</div>
+                    </div>
+                    <div style="padding:2rem"></div>
+                    <div id="credencial" class="grid row">
+                        <div id="logo"></div>
+                        <div><hr /></div>
+                        <div class="txtTarjeta" style="padding:0 0 0 4vw;">${store.getState().autorizacion.usuario ? store.getState().autorizacion.usuario.apellido + " " + store.getState().autorizacion.usuario.nombre : ""}</div>
+                        <div class="txtTarjeta" style="padding:0 0 0 4vw;">${store.getState().autorizacion.usuario ? store.getState().autorizacion.usuario.documento : ""}</div>
+                        <div><hr /></div>
+                        <div class="txtTarjeta" style="padding:2vw 0 4vw 4vw;">Generada: ${new Date().getDate() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getFullYear()}</div>
+                    </div>
+                    <div id="subTituloTexto" class="grid">
+                        <div id="bullet1">${SVGS["BULLET"]}</div>
+                        <div id="solicitud">Se debe presentar DNI.</div>
+                    </div>
+                    <div id="subTituloTexto" class="grid">
+                        <div id="bullet1">${SVGS["BULLET"]}</div>
+                        <div id="solicitud">Presentar último recibo de sueldo.</div>
+                    </div>
+                </div>
+            `;
+        } else {
+            if (store.getState().coberturas.tieneCobertura == "N") {
+                return html`<msgsincobertura-component texto="Volver"></msgsincobertura-component>; `;
+            }
+            if (store.getState().coberturas.tieneCobertura == "E") {
+                return html`<msgnoconeccion-component @click="${this.atras}" texto="Haga click volver" style="cursor:pointer"></msgnoconeccion-component>; `;
+            }
+        }
+    }
+    pdf(e) {
+        let archivo = e.currentTarget.item;
+        window.open(archivo, "_blank");
+        //location.href = archivo
+    }
+    atras() {
+        store.dispatch(goHistoryPrev());
+    }
+    stateChanged(state, name) {
+        if (name == SCREEN || name == MEDIA_CHANGE) {
+            this.mediaSize = state.ui.media.size;
+            this.hidden = true;
+            this.current = state.screen.name;
+            const haveBodyArea = isInLayout(state, this.area);
+            const SeMuestraEnUnasDeEstasPantallas = "-saludCredencial-".indexOf("-" + state.screen.name + "-") != -1;
+            if (haveBodyArea && SeMuestraEnUnasDeEstasPantallas) {
+                this.hidden = false;
+                store.dispatch(getCobertura({ tipo: state.autorizacion.usuario.tipoDocumento, documento: state.autorizacion.usuario.documento }));
+            }
+        }
+        if (name == DATOS_COBERTURA) {
+            this.cobertura = state.coberturas.entities;
+            this.update();
+        }
+        if (name == ERROR_COBERTURA) {
+            this.cobertura = 0;
+            this.update();
+        }
+    }
 
-	static get properties() {
-		return {
-			mediaSize: {
-				type: String,
-				reflect: true,
-				attribute: "media-size",
-			},
-			layout: {
-				type: String,
-				reflect: true,
-			},
-			hidden: {
-				type: Boolean,
-				reflect: true,
-			},
-			area: {
-				type: String,
-			},
-			current: {
-				type: String,
-				reflect: true,
-			},
-		};
-	}
+    static get properties() {
+        return {
+            mediaSize: {
+                type: String,
+                reflect: true,
+                attribute: "media-size",
+            },
+            layout: {
+                type: String,
+                reflect: true,
+            },
+            hidden: {
+                type: Boolean,
+                reflect: true,
+            },
+            area: {
+                type: String,
+            },
+            current: {
+                type: String,
+                reflect: true,
+            },
+        };
+    }
 }
 window.customElements.define("saludcredencial-screen", saludCredencialScreen);
